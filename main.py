565c7cfdafb5ecedc021ff3d134b33006ae12423
Daria Greb <kibergucci@bk.ru>
2024-12-25 01:58:51 +0700

    Добавлен каркас меню

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ def ln_series(x):
 
 
 def main():
-    pass
+    while True:
+        print("\nМеню:"
+              "\n1. Вычислить cos(x)"
+              "\n2. Вычислить ln(1-x)"
+              "\n3. Выйти")
+        choice = input("\nВведите ваш выбор (1/2/3): ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        else:
+            pass
 
 
 if __name__ == "__main__":
